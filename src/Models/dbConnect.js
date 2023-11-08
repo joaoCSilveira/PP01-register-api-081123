818b6999633272b987803dd146852c88365d1629
joaoCSilveira <148132536+joaoCSilveira@users.noreply.github.com>
2023-11-08 12:34:12 -0300

    FIX syntax error dbConnect.js

--- src/Models/dbConnect.js
+++ src/Models/dbConnect.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2/promise")
 require("dotenv").config({path: "../../.env"})
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "kj.sim2!220",
-    database: "cadastro", 
-    port: 3306
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE, 
+    port: process.env.PORTDB
 })
 
 module.exports = db
